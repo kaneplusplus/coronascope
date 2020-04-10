c59de76c5974b09cfea9c7c65c6f037ddad1ce61
kaneplusplus <kaneplusplus@gmail.com>
2020-04-10 16:27:25 -0400

    Added population-adjusted first- and second-derivatives.

--- make-coronascope.r
+++ make-coronascope.r
@@ -27,13 +27,33 @@ make_plot <- function(x) {
   ggplotly(g)
 }
 
+diff_smoothed_last <- function(x, pop, column_name, num_diffs = 1) {
+  totals <- x %>% 
+    group_by(Date) %>%
+    summarize(state_total = sum({{column_name}})) %>%
+    `$`(state_total)
+  
+  totals <- totals / pop
+  
+  for (i in seq_len(num_diffs)) {
+    totals <- diff(totals)
+  }
+  signif(tail(smooth(totals), 1), 3)
+}
+
+pop <- read_csv("pop-est-2019.csv") %>% 
+  select(-County) %>%
+  group_by(State) %>%
+  summarize(Population = sum(Population))
+
 read_csv(paste("https://raw.githubusercontent.com/nytimes",
                "covid-19-data/master/us-counties.csv", sep = "/")) %>%
-  write_csv("us-counties.csv") %>%
   rename(Date = date, County = county, State = state, Cases = cases,
          Deaths = deaths) %>%
+  left_join(pop, by = "State") %>%
+  write_csv("us-counties.csv") %>%
   mutate(Date = anydate(Date)) %>%
-  nest(data = -c(State)) %>%
+  nest(data = -c(State, Population)) %>%
   mutate(total_deaths = map_dbl(data, ~ sum((.x)$Deaths+1, na.rm = TRUE)),
          total_cases = map_dbl(data, ~ sum((.x)$Cases, na.rm = TRUE)),
          steepest_death_curve = 
@@ -44,5 +64,13 @@ read_csv(paste("https://raw.githubusercontent.com/nytimes",
            anydate(map_chr(data, steepest_date, "Deaths")),
          steepest_case_curve_date = 
            anydate(map_chr(data, steepest_date, "Cases")),
+         state_case_1d = 
+           map2_dbl(data, Population, diff_smoothed_last, Cases, 1),
+         state_case_2d = 
+           map2_dbl(data, Population, diff_smoothed_last, Cases, 2),
+         state_death_1d = 
+           map2_dbl(data, Population, diff_smoothed_last, Deaths, 1),
+         state_death_2d = 
+           map2_dbl(data, Population, diff_smoothed_last, Deaths, 2),
          plots = map(data, make_plot)) %>%
   trelliscope(name = "Coronascope", panel_col = "plots", path = "docs")
